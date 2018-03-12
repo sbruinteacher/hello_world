2f7e38d0b696ac0f9cca3652161a8c56ab20fb2d
sbruinteacher <sbruinteacher@gmail.com>
2018-03-12 15:32:50 +0700

    Add files via upload

---
+++ name.py
@@ -0,0 +1,14 @@
+name = input("What is your name?")
+
+
+print("Nice to meet you " + name)
+
+age = int(input("What is your age?"))
+
+print (age * 3)
+
+age = str(age)
+
+print (age + " wow you are sooo old")
+
+
